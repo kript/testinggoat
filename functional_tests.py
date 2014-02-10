7660890c846ad48e3105def412e76829c1ea558f
John Constable <john@kript.net>
2014-02-10 22:30:34 +0000

    typos fixed and beginning of the user story

--- functional_tests.py
+++ functional_tests.py
@@ -5,18 +5,19 @@ class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self): #
 		self.browser = webdriver.Firefox() 
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self): #
+	        self.browser.quit()
 
-        self.browser.quit()
 	def test_can_start_a_list_and_retrieve_it_later(self): #
 	# Edith has heard about a cool new online to-do app. She goes 
 	# to check out its homepage 
-	self.browser.get('http://localhost:8000')
+		self.browser.get('http://localhost:8000')
 
 	# She notices the page title and header mention to-do lists 
-	self.assertIn('To-Do', self.browser.title) # 
-	self.fail('Finish the test!') #
+		self.assertIn('To-Do', self.browser.title) # 
+		self.fail('Finish the test!') #
 
 
 if __name__ == '__main__':
@@ -42,4 +43,3 @@ if __name__ == '__main__':
 # She visits that URL - her to-do list is still there.
 
 # Satisfied, she goes back to sleep
-browser.quit()
